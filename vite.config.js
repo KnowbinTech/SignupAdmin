c7edc96bfd7d7cbb1da3865d017f5448a6c54f23
athulmgeorge <119030486+athulmgeorge@users.noreply.github.com>
2024-10-31 06:53:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 		},
 		proxy:{
 			'/api':{
-				target:"https://manage.signupcasuals.com:8443/api/",
+				target:"https://manage.signupcasuals.com/api/",
 				changeOrigin: true,
 				rewrite: path => path.replace(/^\/api/, ''),
 			}
