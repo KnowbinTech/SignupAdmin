83317df011f5aa4c62d5051e093bf59b31d0577e
Royid Poulose <royidcp96@gmail.com>
2024-11-22 15:27:44 +0530

    url change

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
 		},
 		proxy:{
 			'/api':{
-				// target:"https://manage.signupcasuals.com/api/",
-				target:"http://localhost:8000/api",
+				target:"https://manage.signupcasuals.com/api/",
+				// target:"http://localhost:8000/api",
 				changeOrigin: true,
 				rewrite: path => path.replace(/^\/api/, ''),
 			}
