247a43f840c5d9f643e83f04063249ecd89d3d6f
Royid Poulose <royidcp96@gmail.com>
2024-11-30 16:52:25 +0530

    design error change

--- src/routes/apps/transactions/+page.svelte
+++ src/routes/apps/transactions/+page.svelte
@@ -2,17 +2,16 @@
     import TransactionTable from "./transactionTable.svelte";
   </script>
   
-  <div class="m-3 glow-border">
+  <div class="m-3 shadow-md glow-border">
     <div
-      class=" bg-background glow-border-content text-foreground p-4 px-6"
-      style="height: calc(100vh - 80px);"
+      class="bg-background glow-border-content text-foreground p-4 px-6"
     >
-      <div class="flex items-center justify-between">
-        <h4
+      <div class="flex items-center ">
+        <h2
           class="text-3xl font-bold tracking-tight text-gray-800 dark:text-gray-200 flex-1"
         >
           Transactions
-        </h4>
+        </h2>
       </div>
       <TransactionTable />
     </div>
