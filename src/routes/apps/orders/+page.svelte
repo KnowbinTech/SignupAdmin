d1ef4f1943c919fd6738a4af8cc31fb65a4be8f5
jishnu <jishnuks9421@gmail.com>
2024-12-05 17:00:55 +0530

    static table height removed

--- src/routes/apps/orders/+page.svelte
+++ src/routes/apps/orders/+page.svelte
@@ -6,7 +6,6 @@
 <div class="m-3 glow-border">
   <div
     class=" bg-background glow-border-content text-foreground p-4 px-6"
-    style="height: calc(100vh - 80px);"
   >
     <div class="flex items-center justify-between">
       <h4
