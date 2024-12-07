15ce41c077e48f5bb29238aa45a78fbfc68f2897
jishnu <jishnuks9421@gmail.com>
2024-12-07 14:55:29 +0530

    review table colected

--- src/routes/apps/reviews/reviewTable.svelte
+++ src/routes/apps/reviews/reviewTable.svelte
@@ -1,334 +1,255 @@
 <script lang="ts">
-    import CaretSort from "svelte-radix/CaretSort.svelte";
-    import ChevronDown from "svelte-radix/ChevronDown.svelte";
-    import {
-      createTable,
-      Subscribe,
-      Render,
-      createRender
-    } from "svelte-headless-table";
-    import {
-      addSortBy,
-      addPagination,
-      addTableFilter,
-      addSelectedRows,
-      addHiddenColumns
-    } from "svelte-headless-table/plugins";
-    import { readable } from "svelte/store";
-    import * as Table from "$lib/components/ui/table/index.js";
-    import Actions from "./reviewTableActions.svelte";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-    import { cn } from "$lib/utils.js";
-    import { Input } from "$lib/components/ui/input/index.js";
-    import DataTableCheckbox from "./reviewTableCheckbox.svelte";
-   
-    type Payment = {
-      orderId: string;
-      customerName: string;
-      orderDate: string;
-      orderStatus: "Pending" | "Processing" | "Success" | "Failed";
-      amount: number;
-      paymentStatus: "Pending" | "Success" | "Failed";
-      paymentMethod: "Cash" | "Credit Card" | "Debit Card" | "Paypal" | "Other";
-      email: string;
+  import CaretSort from "svelte-radix/CaretSort.svelte";
+  import ChevronDown from "svelte-radix/ChevronDown.svelte";
+  import * as Table from "$lib/components/ui/table/index.js";
+  import { Button } from "$lib/components/ui/button";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import API from "$lib/services/api";
+  import { MoreHorizontal } from "lucide-svelte";
+  import { toast } from "svelte-sonner";
+  import ConfirmDeleteModal from "$lib/components/ui/confirmation-modal/ConfirmDeleteModal.svelte";
+  import Pagination from "$lib/components/ui/table-pagination/pagination.svelte";
+  import { createEventDispatcher, onMount } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let showForm: boolean = false;
+  // variables to handle pagination and table details
+  let page: number = 1;
+  let totalItems: number;
+  let per_page: number = 10;
+  let tableData: any[] = [];
+  let sortData: boolean = true;
+  let sortField: string = "";
+  let searchData: string = "";
+
+  let showDeleteModal = false;
+  let deletingReview: any;
+
+  let editData: any;
+  let editForm: boolean = false;
+
+  let hidableCoulumns: any[] = [];
+
+  async function getReviews() {
+    try {
+      let res = await API.get(
+        `/customer/manage/reviews/?page=${page}&per_page=${per_page}&ordering=${sortField}&search=${searchData}`
+      );
+      totalItems = res.data.total;
+      tableData = res.data.results;
+      hidableCoulumns = res.data.columns
+    } catch (error) {
+      console.error("fetch:reviews:", error);
+      return [];
+    }
+  }
+
+  async function getNextPage() {
+    page += 1;
+    await getReviews();
+  }
+
+  async function getPage(event: any) {
+    page = event.detail;
+    await getReviews();
+  }
+
+  async function getPreviousPage() {
+    page -= 1;
+    await getReviews();
+  }
+
+  function onEdit(data: any) {
+    editData = data;
+    showForm = true;
+    editForm = true;
+  }
+
+  function handleNewReview() {
+    editData = null;
+    editForm = false;
+    showForm = false;
+    getReviews();
+  }
+
+  function onDelete(id: any, name: any) {
+    showDeleteModal = true;
+    deletingReview = {
+      id: id,
+      name: name,
     };
-   
-    const data: Payment[] = [
-      {
-        orderId: "m5gr84i9",
-        customerName: "Manoj Kumar",
-        orderDate: "2021-08-10",
-        amount: 316,
-        orderStatus: "Success",
-        paymentStatus: "Pending",
-        paymentMethod: "Cash",
-        email: "ken99@yahoo.com"
-      },
-      {
-        orderId: "3u1reuv4",
-        customerName: "Kudumon Potty",
-        orderDate: "2021-08-10",
-        amount: 242,
-        orderStatus: "Success",
-        paymentStatus: "Pending",
-        paymentMethod: "Cash",
-        email: "Abe45@gmail.com"
-      },
-      {
-        orderId: "derv1ws0",
-        customerName: "Suresh Gopi",
-        orderDate: "2021-08-10",
-        amount: 837,
-        orderStatus: "Processing",
-        paymentStatus: "Pending",
-        paymentMethod: "Cash",
-        email: "Monserrat44@gmail.com"
-      },
-      {
-        orderId: "5kma53ae",
-        customerName: "Vimala Rajedran",
-        orderDate: "2021-08-10",
-        amount: 874,
-        orderStatus: "Success",
-        paymentStatus: "Pending",
-        paymentMethod: "Cash",
-        email: "Silas22@gmail.com"
-      },
-      {
-        orderId: "bhqecj4p",
-        customerName: "Kunjilakshmi Ammal",
-        orderDate: "2021-08-10",
-        amount: 721,
-        orderStatus: "Failed",
-        paymentStatus: "Pending",
-        paymentMethod: "Cash",
-        email: "carmella@hotmail.com"
-      }
-    ];
-   
-    const table = createTable(readable(data), {
-      sort: addSortBy({ disableMultiSort: true }),
-      page: addPagination(),
-      filter: addTableFilter({
-        fn: ({ filterValue, value }) => value.includes(filterValue)
-      }),
-      select: addSelectedRows(),
-      hide: addHiddenColumns()
-    });
-   
-    const columns = table.createColumns([
-      table.column({
-        header: (_, { pluginStates }) => {
-          const { allPageRowsSelected } = pluginStates.select;
-          return createRender(DataTableCheckbox, {
-            checked: allPageRowsSelected
-          });
-        },
-        accessor: "orderId",
-        cell: ({ row }, { pluginStates }) => {
-          const { getRowState } = pluginStates.select;
-          const { isSelected } = getRowState(row);
-   
-          return createRender(DataTableCheckbox, {
-            checked: isSelected
-          });
-        },
-        plugins: {
-          sort: {
-            disable: true
-          },
-          filter: {
-            exclude: true
-          }
-        }
-      }),
-      table.column({
-        header: "Order ID",
-        accessor: ({ orderId }) => orderId,
-        plugins: { sort: { disable: true }, filter: { exclude: true } }
-      }),
-      table.column({
-        header: "Customer Name",
-        accessor: "customerName",
-        cell: ({ value }) => value.toLowerCase(),
-        plugins: {
-          filter: {
-            getFilterValue(value) {
-              return value.toLowerCase();
-            }
-          }
-        }
-      }),
-      table.column({
-        header: "Order Date",
-        accessor: "orderDate",
-        plugins: { sort: { disable: true }, filter: { exclude: true } }
-      }),
-      table.column({
-        header: "Order Status",
-        accessor: "orderStatus",
-        plugins: { sort: { disable: true }, filter: { exclude: true } }
-      }),
-      table.column({
-        header: "Payment Status",
-        accessor: "paymentStatus",
-        plugins: { sort: { disable: true }, filter: { exclude: true } }
-      }),
-      table.column({
-        header: "Payment Method",
-        accessor: "paymentMethod",
-        plugins: { sort: { disable: true }, filter: { exclude: true } }
-      }),
-      table.column({
-        header: "Total Amount",
-        accessor: "amount",
-        cell: ({ value }) => {
-          const formatted = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "INR"
-          }).format(value);
-          return formatted;
-        },
-        plugins: {
-          sort: {
-            disable: true
-          },
-          filter: {
-            exclude: true
-          }
-        }
-      }),
-      table.column({
-        header: "",
-        accessor: ({ orderId }) => orderId,
-        cell: (item) => {
-          return createRender(Actions, { id: item.value });
-        },
-        plugins: {
-          sort: {
-            disable: true
-          }
-        }
+  }
+
+  function confirmDelete() {
+    API.delete(`/masterdata/review/${deletingReview.id}/delete_record/`)
+      .then((response) => {
+        closeDeleteModal();
+        getReviews();
+        toast(response.data.message);
       })
-    ]);
-   
-    const {
-      headerRows,
-      pageRows,
-      tableAttrs,
-      tableBodyAttrs,
-      flatColumns,
-      pluginStates,
-      rows
-    } = table.createViewModel(columns);
-   
-    const { sortKeys } = pluginStates.sort;
-   
-    const { hiddenColumnIds } = pluginStates.hide;
-    const ids = flatColumns.map((c) => c.id);
-    let hideForId = Object.fromEntries(ids.map((id) => [id, true]));
-   
-    $: $hiddenColumnIds = Object.entries(hideForId)
-      .filter(([, hide]) => !hide)
-      .map(([id]) => id);
-   
-    const { hasNextPage, hasPreviousPage, pageIndex } = pluginStates.page;
-    const { filterValue } = pluginStates.filter;
-   
-    const { selectedDataIds } = pluginStates.select;
-   
-    const hideableCols = ["orderStatus", "orderDate", "customerName", "amount", "paymentStatus", "paymentMethod"];
-  </script>
-   
-  <div class="w-full p-4 bg-background text-foreground">
-    <div class="mb-4 flex items-center gap-4">
+      .catch((error) => {
+        console.error("Error deleting Review:", error);
+        closeDeleteModal();
+        toast(error.response.data.message);
+      });
+  }
+
+  function closeDeleteModal() {
+    showDeleteModal = false;
+  }
+
+  function cancelEditModel() {
+    editData = null;
+    editForm = false;
+    showForm = false;
+    dispatch("cancel");
+  }
+
+  onMount(() => {
+    getReviews();
+  });
+
+  function sortName() {
+    if (sortData) {
+      sortField = "name";
+      getReviews();
+      sortData = !sortData;
+    } else {
+      sortField = "-name";
+      getReviews();
+      sortData = !sortData;
+    }
+  }
+
+  function searchName(event: any) {
+    searchData = event.target.value;
+    getReviews();
+  }
+
+  function pageLimit(event: any, value: any) {
+    per_page = value;
+    getReviews();
+  }
+</script>
+
+<div>
+  {#if showDeleteModal}
+    <ConfirmDeleteModal
+      attribute={deletingReview.name}
+      on:confirm={confirmDelete}
+      on:cancel={closeDeleteModal}
+    />
+  {/if}
+</div>
+
+
+<div class="w-full p-5">
+  <div class="my-2 flex justify-between">
+    <div>
       <Input
         class="max-w-sm"
-        placeholder="Filter Review..."
+        placeholder="Search Reviews..."
         type="text"
-        bind:value={$filterValue}
+        on:input={(event) => searchName(event)}
       />
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger asChild let:builder>
-          <Button variant="outline" class="ml-auto" builders={[builder]}>
-            Columns <ChevronDown class="ml-2 h-4 w-4" />
-          </Button>
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content>
-          {#each flatColumns as col}
-            {#if hideableCols.includes(col.id)}
-              <DropdownMenu.CheckboxItem bind:checked={hideForId[col.id]}>
-                {col.header}
-              </DropdownMenu.CheckboxItem>
-            {/if}
-          {/each}
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
     </div>
-    <div class="rounded-md border">
-      <Table.Root {...$tableAttrs}>
-        <Table.Header>
-          {#each $headerRows as headerRow}
-            <Subscribe rowAttrs={headerRow.attrs()}>
-              <Table.Row>
-                {#each headerRow.cells as cell (cell.id)}
-                  <Subscribe
-                    attrs={cell.attrs()}
-                    let:attrs
-                    props={cell.props()}
-                    let:props
-                  >
-                    <Table.Head
-                      {...attrs}
-                      class={cn("[&:has([role=checkbox])]:pl-3")}
-                    >
-                      {#if cell.id === "amount"}
-                        <div class="text-right">
-                          <Render of={cell.render()} />
-                        </div>
-                      {:else if cell.id === "customerName"}
-                        <Button variant="ghost" on:click={props.sort.toggle}>
-                          <Render of={cell.render()} />
-                          <CaretSort
-                            class={cn(
-                              $sortKeys[0]?.id === cell.id && "text-foreground",
-                              "ml-2 h-4 w-4"
-                            )}
-                          />
-                        </Button>
-                      {:else}
-                        <Render of={cell.render()} />
-                      {/if}
-                    </Table.Head>
-                  </Subscribe>
-                {/each}
-              </Table.Row>
-            </Subscribe>
-          {/each}
-        </Table.Header>
-        <Table.Body {...$tableBodyAttrs}>
-          {#each $pageRows as row (row.id)}
-            <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-              <Table.Row
-                {...rowAttrs}
-                data-state={$selectedDataIds[row.id] && "selected"}
+    <div class="flex">
+      <div class="mr-2">
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Button variant="outline" size="sm">
+              Column
+              <ChevronDown class="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            {#each hidableCoulumns as column}
+              <DropdownMenu.CheckboxItem bind:checked={column.is_default}
+                >{column.text}</DropdownMenu.CheckboxItem
               >
-                {#each row.cells as cell (cell.id)}
-                  <Subscribe attrs={cell.attrs()} let:attrs>
-                    <Table.Cell class="[&:has([role=checkbox])]:pl-3" {...attrs}>
-                      {#if cell.id === "amount"}
-                        <div class="text-right font-medium">
-                          <Render of={cell.render()} />
-                        </div>
-                      {:else}
-                        <Render of={cell.render()} />
-                      {/if}
-                    </Table.Cell>
-                  </Subscribe>
-                {/each}
-              </Table.Row>
-            </Subscribe>
-          {/each}
-        </Table.Body>
-      </Table.Root>
-    </div>
-    <div class="flex items-center justify-end space-x-2 py-4">
-      <div class="flex-1 text-sm text-muted-foreground">
-        {Object.keys($selectedDataIds).length} of{" "}
-        {$rows.length} row(s) selected.
+            {/each}
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </div>
-      <Button
-        variant="outline"
-        size="sm"
-        on:click={() => ($pageIndex = $pageIndex - 1)}
-        disabled={!$hasPreviousPage}>Previous</Button
-      >
-      <Button
-        variant="outline"
-        size="sm"
-        disabled={!$hasNextPage}
-        on:click={() => ($pageIndex = $pageIndex + 1)}>Next</Button
-      >
+      <div>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Button variant="outline" size="sm">
+              {per_page}
+              <ChevronDown class="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Item on:click={(event) => pageLimit(event, 10)}
+              >10</DropdownMenu.Item
+            >
+            <DropdownMenu.Item on:click={(event) => pageLimit(event, 20)}
+              >20</DropdownMenu.Item
+            >
+            <DropdownMenu.Item on:click={(event) => pageLimit(event, 25)}
+              >25</DropdownMenu.Item
+            >
+            <DropdownMenu.Item on:click={(event) => pageLimit(event, 50)}
+              >50</DropdownMenu.Item
+            >
+            <DropdownMenu.Item on:click={(event) => pageLimit(event, 100)}
+              >100</DropdownMenu.Item
+            >
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </div>
     </div>
   </div>
+  <Table.Root>
+    <Table.Header>
+      <Table.Row>
+        {#each hidableCoulumns as column}
+          {#if column.is_default}
+            <Table.Head>{column.text}</Table.Head>
+          {/if}
+        {/each}
+      </Table.Row>
+    </Table.Header>
+    {#each tableData as data}
+      <Table.Row>
+        <Table.Cell>{data.name}</Table.Cell>
+
+        {#each hidableCoulumns as column}
+          {#if column.is_default}
+            <Table.Cell>{data[column.value]}</Table.Cell>
+          {/if}
+        {/each}
+        <!-- <Table.Cell>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild let:builder>
+              <Button builders={[builder]} variant="ghost"
+                ><MoreHorizontal class="h-4 w-4" /></Button
+              >
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content class="absolute">
+              <DropdownMenu.Item on:click={() => onEdit(data)}
+                ><i class="fa fa-pencil sm mr-2"></i>Edit</DropdownMenu.Item
+              >
+              <DropdownMenu.Item on:click={() => onDelete(data.id, data.name)}
+                ><i class="fa fa-trash sm mr-2" style="color:red"
+                ></i>Delete</DropdownMenu.Item
+              >
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </Table.Cell> -->
+      </Table.Row>
+    {/each}
+  </Table.Root>
+
+  <div class="d-flex justify-end align-items-end mt-3">
+    <Pagination
+      {totalItems}
+      {per_page}
+      on:prev={getPreviousPage}
+      on:next={getNextPage}
+      on:page={(event) => getPage(event)}
+    />
+  </div>
+</div>
